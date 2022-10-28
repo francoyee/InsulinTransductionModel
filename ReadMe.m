d0bdbf49334916611ffb2a7142afc2e6a60ed256
Gianfranco Yee <51855261+francoyee@users.noreply.github.com>
2022-10-28 11:57:37 -0400

    Add files via upload

---
+++ ReadMe.m
@@ -0,0 +1,56 @@
+% STATE VARIABLE
+% x(1)   Insulin input
+% x(2)   Concentration of unbound surface insulin receptors
+% x(3)   Concentration of unphosphorylated once-bound surface receptors
+% x(4)   Concentration of phosphorylated twice-bound surface receptors
+% x(5)   Concentration of phosphorylated once-bound surface receptors
+% x(6)   Concentration of unbound unphosphorylated intracellular receptors
+% x(7)   Concentration of phosphorylated twice-bound intracellular receptors
+% x(8)   Concentration of phosphorylated once-bound intracellular receptors
+% x(9)   Concentration of unphosphorylated IRS-1
+% x(10)  Concentration of tyrosine-phosphorylated IRS-1
+% x(11)  Concentration of unactivated PI 3-kinase
+% x(12)  Concentration of tyrosine-phosphorylated IRS-1/activated PI 3-kinase complex
+% x(13)  Percentage of PI(3,4,5)P3 out of the total lipid population
+% x(14)  Percentage of PI(4,5)P2 out of the total lipid population
+% x(15)  Percentage of PI(3,4)P2 out of the total lipid population
+% x(16)  Percentage of unactivated Akt
+% x(17)  Percentage of activated Akt
+% x(18)  Percentage of unactivated PKC-L
+% x(19)  Percentage of activated PKC-L
+% x(20)  Percentage of intracellular GLUT4
+% x(21)  Percentage of cell surface GLUT4
+% x(22)  Concentration of serine phosphorylated IRS-1
+
+
+% Fig 6:    Model Time Course Simulations Without Feedback
+% Axes:     Time (min) vs. Concentration (pM)
+% A:        Free unbound surface receptors (x2)
+% B:        Once-bound (x5) and twice-bound phosphorylated surface receptors (x4)
+% C:        Total phosphorylated surface receptors (x4 + x5)
+% D:        Unphosphorylated IRS-1 (x9) and tyrosine-phosphorylated IRS-1 (x10)
+
+
+% Fig 7:    Model Time Course Simulations Without Feedback
+% Axes:     Time (min) vs. Concentration (fM)
+% A:        Activated PI 3-kinase (x12)
+% B:        Phosphatidylinositol 3,4,5-trisphosphate [PI(3,4,5)P3] (x13) and phosphatidylinositol 3,4-bisphosphate PI(3,4)P2] (x15)
+% Axes:     Time (min) vs. Percentage
+% C:        Activated PKC-L (x19)
+% D:        Cell surface GLUT4 (x21)
+
+
+% Fig 8:    Model Time Course Simulations With Feedback
+% Axes:     Time (min) vs. Concentration (pM)
+% A:        Free unbound surface receptors (x2)
+% B:        Once-bound (x5) and twice-bound phosphorylated surface receptors (x4)
+% C:        Total phosphorylated surface receptors (x4 + x5)
+% D:        Unphosphorylated IRS-1 (x9), tyrosine-phosphorylated IRS-1 (x10), serine-phosphorylated IRS-1 (x10a)
+
+
+% Fig 9:    Model Time Course Simulations With Feedback
+% A:        Activated PI 3-kinase (x12)
+% B:        Phosphatidylinositol 3,4,5-trisphosphate [PI(3,4,5)P3] (x13) and phosphatidylinositol 3,4-bisphosphate PI(3,4)P2] (x15)
+% Axes:     Time (min) vs. Percentage
+% C:        Activated PKC-L (x19)
+% D:        Cell surface GLUT4 (x21)
